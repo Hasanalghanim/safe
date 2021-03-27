b836e29201c79b78fdf6451e3f70119ae0a62fc7
hasan <hasanalghanim@gmail.com>
2021-03-26 21:25:26 -0600

    Form and Model update
    Models are completed and form takes in from models + saves to database.

--- safe/main/models.py
+++ safe/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models.fields import DateTimeField
 
 # Create your models here.
+
+
+class walkrequest(models.Model):
+    name            = models.CharField(max_length=100, blank=False)
+    phone           = models.IntegerField()
+    fromlocation    = models.CharField(max_length=100, blank=False)
+    tolocation      = models.CharField(max_length=100, blank=False)
+    timerecived     = models.DateTimeField(auto_now_add=True)
+    completed       = models.BooleanField(blank=True)
+
+
+    def __str__(self):
+        return self.name
--- safe/main/views.py
+++ safe/main/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from .forms import walkrequestform
 
 # Create your views here.
 
 
 def home(request):
-    return render(request,'main/home.html')
+    form = walkrequestform()
+
+    if request.method == "POST":
+        form = walkrequestform(request.POST)
+        if form.is_valid():
+            form = form.save()
+            data = "form saved"
+        return render(request,'main/home.html', {'data':data, 'form':form })
+
+    if request.method == "GET":
+        form = walkrequestform()
+        data = "do the form"
+        return render(request,'main/home.html', {'data':data, 'form':form })
